3b1a9877cf3cb08340cb06d2ac9b4de9f3802f3e
Rathina Boopathi <rath.1911180@gct.ac.in>
2021-10-23 17:01:37 +0530

    name changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import {Card, Button } from 'antd'
+import Meta from 'antd/lib/card/Meta';
+
+
 
 function App() {
+  const [data, setData]= useState([]);
+  useEffect(()=>{
+    const loadNews= async()=>{
+      const resp = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=18dde8a47aa34ac8b657a9c7a1a76592")
+      setData(resp.data.articles);
+    };
+    loadNews();
+  },[]);
+  console.log(data, "NEWS")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome to SRB's news page</h1>
+      <h3>Done using NewsAPI</h3>
+     {data && data.map((item,index)=>{
+       return (
+         <div>
+          <div key= {index} hoverable className='wrapper' >
+           <img alt='news' src={item.urlToImage}/>
+           <h3>{item.title}</h3>
+           <p>{item.description}</p>
+           <Button href={item.urlToImage} type='primary'>Read More</Button>
+          </div> 
+         </div>)
+     }) }
     </div>
-  );
+  )
 }
+
+  
+
 
 export default App;
