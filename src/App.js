9c24a384946e57d6ba9c232ef36d5ae096a29c10
Rathina Boopathi <rath.1911180@gct.ac.in>
2021-10-25 22:56:04 +0530

    name changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import {Card, Button } from 'antd'
-import Meta from 'antd/lib/card/Meta';
+import React from 'react';
+import classes from './App.module.css';
+import Home from './components/Home.js';
 
-
-
-function App() {
-  const [data, setData]= useState([]);
-  useEffect(()=>{
-    const loadNews= async()=>{
-      const resp = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=18dde8a47aa34ac8b657a9c7a1a76592")
-      setData(resp.data.articles);
-    };
-    loadNews();
-  },[]);
-  console.log(data, "NEWS")
-
-  return (
-    <div className='App'>
-      <h1>Welcome to SRB's news page</h1>
-      <h3>Done using NewsAPI</h3>
-     {data && data.map((item,index)=>{
-       return (
-         <div>
-          <div key= {index} hoverable className='wrapper' >
-           <img alt='news' src={item.urlToImage}/>
-           <h3>{item.title}</h3>
-           <p>{item.description}</p>
-           <Button href={item.urlToImage} type='primary'>Read More</Button>
-          </div> 
-         </div>)
-     }) }
+function App(){
+  return(
+    <div className={classes.body}>
+      <Home/>
     </div>
   )
 }
---
+++ src/components/Apple.js
@@ -0,0 +1,34 @@
+import React, {UseState, UseEffect} from 'react'
+import axios from 'axios';
+import classes from './tech.module.css';
+
+function Apple() {
+  const [News, setData] = UseState([]);
+  UseEffect(()=>{
+    const loadNews= async()=>{
+      const resp = await axios.get("https://newsapi.org/v2/everything?q=apple&from=2021-10-23&to=2021-10-23&sortBy=popularity&apiKey=18dde8a47aa34ac8b657a9c7a1a76592")
+      setData(resp.data.articles);
+    };
+    loadNews();
+  },[]);
+  console.log(News, "NEWS")
+
+  return (
+    <div>
+      
+     {News && News.map((item)=>{
+       return (
+         <div>
+          <div className={classes.wrapper} >
+           <img alt='news' src={item.urlToImage}/>
+           <h3>{item.title}</h3>
+           <p>{item.description}</p>
+           <button type='primary'>Read More</button>
+          </div> 
+         </div>)
+     }) }
+    </div>
+  )
+}
+
+export default Apple
