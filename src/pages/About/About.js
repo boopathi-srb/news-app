ec78f21445c228852a148cba528d3a7cb9aacfd7
Rathina Boopathi <rath.1911180@gct.ac.in>
2021-11-12 07:37:20 +0530

    name changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import classes from "../../App.module.css"
 
 export class About extends Component {
   render() {
     return (
       <div>
-        This is a news app created using NewsAPI and React framework.
+       <h1 className={classes.About_title}>About</h1>
+       <p>
+         This News App is created using NewsAPI and React framework.
+       </p>
+       <p>
+          My previous project: 
+          <a href="www.amazon-page-by-srb.vercel.app">  Smart watch</a>
+       </p>
+
       </div>
     )
   }
 }
 
-export default About
+export default About 
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import classes from "../../App.module.css"
 
 export class Contact extends Component {
   render() {
     return (
       <div>
-        contact page
+       <h1 className={classes.About_title}>Contact page</h1> 
+       
       </div>
     )
   }
